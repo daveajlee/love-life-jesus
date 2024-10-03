b159d3e5c8fca72a9ea7cbb20bf46590b14e3f73
David A J Lee <dave@davelee.de>
2024-10-03 15:07:58 +0200

    Add logos for spotify, apple and youtube

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 import MediaQuery from "react-responsive";
 import logo from "./../assets/img/logo-large.png"
 import logoSmall from "./../assets/img/logo-large-mobile.png"
@@ -7,6 +7,9 @@ import episodesImage from './../assets/img/bible-coffee-unsplash.png';
 import subscribeImage from './../assets/img/subscribe-unsplash.png';
 import fellowshipImage from './../assets/img/fellowship-unsplash.png';
 import contactImage from './../assets/img/contact-unsplash.png';
+import podcastAppleLogo from './../assets/img/Apple_Podcasts_Icon_RGB_lg_060623.svg';
+import podcastSpotifyLogo from './../assets/img/Spotify_Primary_Logo_RGB_Black.png';
+import podcastYoutubeLogo from './../assets/img/yt_logo_rgb_light.png';
 
 const Home = () => (
     <Container fluid>
@@ -37,8 +40,13 @@ const Home = () => (
                 podcast and find out. I promise each episode will help you to get know Jesus more.</p></Col>
         </Row>
         <Row>
-            <Col className="text-left mt-3" style={{color: 'white'}}>
-            Icons and links to where you can get podcast</Col>
+            <Col className="text-center">
+                <a href="https://podcasts.apple.com/us/podcast/love-life-jesus/id1769064498"><Image src={podcastAppleLogo} alt="Listen on Apple Podcasts" width="100" rounded/></a>
+                <span style={{marginRight: "30px"}}/>
+                <a href="https://open.spotify.com/show/0rmI1Y4Cgk0c8YqhV5MjoM"><Image src={podcastSpotifyLogo} alt="Listen on Spotify Podcasts" width="100" rounded/></a>
+                <span style={{marginRight: "30px"}}/>
+                <a href=""><Image src={podcastYoutubeLogo} alt="Listen on Youtube" width="300" rounded/></a>
+            </Col>
         </Row>
 
         <hr/>
