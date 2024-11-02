73dfcf72d3ac1df64d5e895c0ca6cab885277316
David A J Lee <dave@davelee.de>
2024-11-02 15:30:23 +0100

    Add episode 1 information

--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -16,11 +16,15 @@ const Episodes = () => (
                     <Accordion.Item eventKey="0">
                         <Accordion.Header>Prayer – thanking, praising and asking God</Accordion.Header>
                         <Accordion.Body>
-                            <a target="_blank" rel="noreferrer" href="https://www.biblegateway.com/passage/?search=Psalm%20117&version=NIV">Psalm 117 (view in Bible Gateway)</a>
+                            <a target="_blank" rel="noreferrer" href="https://podcasters.spotify.com/pod/show/dajlee85/episodes/Prayer---thanking--praising-and-asking-God-e2qf9ju">Listen to the episode on Spotify (opens in new window)</a>
                             <br/><br/>
-                            Coming Soon....
+                            <a target="_blank" rel="noreferrer" href="https://www.biblegateway.com/passage/?search=Psalm%20117&version=NIV">Psalm 117 (view on Bible Gateway in new window)</a>
                             <br/><br/>
-                            <button><img src={pdfLogo} alt="PDF logo" width={60}/></button>
+                            Prayer plays an important role in living a life with and for Jesus. We start this series on prayer by looking at an essential element of prayer which is often overlooked - thanking and praising God before we ask God for wisdom.
+Psalm 117 encourages us to always praise God. It provides a short and easy to remember structure that we can use at the beginning of our prayers. 
+In this episode, we will study Psalm 117 and at the end I will offer two tips how you can integrate Psalm 117 into your prayer life.
+                            <br/><br/>
+                            <a href="./../assets/pdf/Series1Episode1.pdf"><img src={pdfLogo} alt="PDF logo" width={60}/></a>
                         </Accordion.Body>
                     </Accordion.Item>
                 </Accordion>
